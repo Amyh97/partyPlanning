a207b3c4e03230c01eda6dcabd1e9c1cb0b690ba
Amyh97 <amy.hollis@btinternet.com>
2021-05-24 11:23:17 +0000

    create task model and render view

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'notes',
+        'complete',
+        'person',
+    )
+
+
+admin.site.register(task, TaskAdmin)
--- home/views.py
+++ home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import task
 
 
 def index(request):
     """ a view to return index page """
-    return render(request, 'home/index.html')
+
+    tasks = task.objects.filter(complete=False)
+
+    context = {
+        'tasks': tasks,
+    }
+    return render(request, 'home/index.html', context)
